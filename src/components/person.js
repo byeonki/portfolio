489861825044720559bc9e3b2028538e1b11cc59
Nathen So <jhso@zigbang.com>
2023-07-30 20:35:57 +0900

    add about

---
+++ src/components/person.js
@@ -0,0 +1,42 @@
+import React, { useMemo, useState } from 'react'
+import Container from './container'
+import * as styles from './person.module.css'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import InlineImages from '../components/inline-images'
+import { BLOCKS, INLINES } from '@contentful/rich-text-types'
+
+const Person = ({ person }) => {
+	console.log("person", person)
+	if(!person){
+		return null
+	}
+
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+        const images = node?.data?.target?.images
+        return images ? <InlineImages images={images} /> : null
+      },
+    },
+  }
+
+  return (
+    <Container>
+			<div className={styles.content}>
+				<div className={styles.wrap}>
+					{renderRichText(person.shortBio, options)}
+				</div>
+				<div className={styles.wrap}>
+					<div className={styles.box}>
+						{renderRichText(person.client, options)}
+					</div>
+					<div className={styles.box}>
+						{renderRichText(person.exh, options)}
+					</div>
+				</div>
+			</div>
+    </Container>
+  )
+}
+
+export default Person
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,42 @@
 import React from 'react'
-
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
 import Layout from '../components/layout'
-
+import Person from '../components/person'
 class AboutIndex extends React.Component {
   render() {
+    const persons = get(this, 'props.data.allContentfulPerson.nodes')
+
+    if(!persons?.length){
+      return null
+    }
+
     return (
       <Layout location={this.props.location}>
-        <span>about</span>
+        <Person person={persons[0]} />
       </Layout>
     )
   }
 }
 
 export default AboutIndex
+
+export const pageQuery = graphql`
+  query PersonQuery {
+    allContentfulPerson(sort: { name: DESC }) {
+      nodes {
+        name
+        title
+        shortBio {
+          raw
+        }
+        client {
+          raw
+        }
+        exh {
+          raw
+        }
+      }
+    }
+  }
+`
