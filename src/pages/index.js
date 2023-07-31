76d2df026e9b34efebed88c9c7e8f755102d878f
Nathen So <jhso@zigbang.com>
2023-07-31 19:16:27 +0900

    change range

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default RootIndex
 
 export const pageQuery = graphql`
   query HomeQuery {
-    allContentfulBlogPost(sort: { publishDate: DESC }) {
+    allContentfulBlogPost(sort: { publishDate: ASC }) {
       nodes {
         title
         slug
